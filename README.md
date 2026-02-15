f24b20dcc2bf8355237917a2056220c57449187f
Sannteen <143964654+Sannteen@users.noreply.github.com>
2026-02-15 16:36:03 -0500

    New Assignment

---
+++ README.md
@@ -0,0 +1,3 @@
+Assignment 1 Module  1
+Lecturer:  Doron Williams 
+Contributors: Sannteen Budda and Shanna-Deen Knight
---
+++ quest1.js
@@ -0,0 +1,9 @@
+// declare the two variables
+let x = 5;
+ let y = 10;
+
+ // function to add the two numbers
+ let sum = x + y;
+
+// Display the result inside the paragraph
+document.getElementById("demo").innerHTML = "The sum of x and y is: " + sum;
